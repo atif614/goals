c525c8eeeccf06d32765887573bfb919e91d6b61
Atif Raza <razaatif20786@gmail.com>
2023-05-07 22:31:33 +0530

    Initial Commit

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,58 +4,58 @@ const asyncHandler = require('express-async-handler');
 const User = require('../model/userModel');
 
 const loginUser = asyncHandler(async (req, res) => {
-    const {email,password} = req.body;
+    const { email, password } = req.body;
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
-    if(user && (await bcrypt.compare(password,user.password))){
-       res.json({
-        _id:user.id,
-        name:user.name,
-        email:user.email,
-        token:generateToken(user._id)
-        }) 
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
     }
-    else{
+    else {
         res.status(400)
-        throw new Error('Invalid Credentials'); 
+        throw new Error('Invalid Credentials');
     }
     // res.json({ message: "Login User" })
 })
 
 const registerUser = asyncHandler(async (req, res) => {
     console.log(req.body);
-    const {name,email,password} = req.body;
-    if(!name || !email || !password){
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
         res.status(400)
         throw new Error('Please Add all Fields')
     }
     // To Check user Exist
-    const userExists = await User.findOne({email});
-    if(userExists){
+    const userExists = await User.findOne({ email });
+    if (userExists) {
         res.status(400)
         throw new Error('User already Exist');
     }
 
     // Hash Password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-   //Create user
-   const user = await User.create({
-    name,
-    email,
-    password:hashedPassword
-   }) 
-   if(user){
-    res.status(201).json({
-        _id:user.id,
-        name:user.name,
-        email:user.email,
-        token:generateToken(user._id)
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
     })
-}
-    else{
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    }
+    else {
         res.status(400)
         throw new Error('Invalid user data')
     }
@@ -67,8 +67,8 @@ const getMe = asyncHandler(async (req, res) => {
 
 // Generate Token using JWT
 const generateToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:'30d'
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
     })
 }
 module.exports = {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, getMe } = require('../controllers/userControllers');
-
+const { protect } = require('../middlware/authMiddleware')
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/', getMe);
+router.get('/getMe', protect, getMe);
 
 module.exports = router;
