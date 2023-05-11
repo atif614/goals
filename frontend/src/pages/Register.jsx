bfdfed7b18336c6c7407bac2e69a5dee7b33ae50
Atif Raza <razaatif20786@gmail.com>
2023-05-11 23:47:39 +0530

    Initial commit

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,56 @@
+import e from "express";
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+  const onChange = () => {
+    console.log(formData);
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+    console.log(formData);
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please Create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="text" className="form-control" id="name" value={name} placeholder="Enter your name" onChange={onChange} />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="email" value={email} placeholder="Enter your Email" onChange={onChange} />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="password" value={password} placeholder="Enter your password" onChange={onChange} />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="password2" value={password2} placeholder="Enter your password2" onChange={onChange} />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
+export default Register;
